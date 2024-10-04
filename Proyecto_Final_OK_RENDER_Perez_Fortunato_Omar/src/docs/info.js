f2a17b34ebe6ebb063d9c0d83ae06e9666d374b1
omarpf86 <omarpf86@gmail.com>
2024-10-04 00:04:15 -0300

    Update info.js

--- Proyecto_Final_OK_RENDER_Perez_Fortunato_Omar/src/docs/info.js
+++ Proyecto_Final_OK_RENDER_Perez_Fortunato_Omar/src/docs/info.js
@@ -8,7 +8,7 @@ export const info = {
         },
         servers: [
             {
-                url: "http://localhost:8080",
+                url: "https://proyecto-final-ok-render-perez-fortunato.onrender.com",
             }
             
         ],
